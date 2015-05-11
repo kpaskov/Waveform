956dbe273a658f04426a6766fbe8e14ceefb6456
Kelley <kpaskov@stanford.edu>
2015-05-10 19:02:57 -0700

    Added code to sample the MIMIC data so that maximum three alarms of the same type are pulled from a single patient.

--- src/query_data/get_mimic_data.m
+++ src/query_data/get_mimic_data.m
@@ -2,11 +2,15 @@ directory = '/Users/kelley/Data/Waveform/MIMICII/';
 match_data = load('Mimic2dbMAP.mat');
 match_data = match_data.Mimic2dbMAP;
 
-for i=1:size(match_data, 1)
-    record_id = match_data(i, 1);
-    record_id = record_id{1};
-    disp(record_id);
+for i=43:size(match_data, 1)
+    try
+        record_id = match_data(i, 1);
+        record_id = record_id{1};
+        disp(record_id);
 
-    get_record_data(record_id, directory);
+        get_record_data(record_id, directory);
+    catch Error
+        display(Error);
+    end
 end
 
---
+++ src/query_data/sample_records.py
@@ -0,0 +1,53 @@
+from shutil import copyfile
+from random import shuffle
+import os
+
+__author__ = 'kelley'
+
+mimic_directory = '/Users/kelley/Data/Waveform/MIMICII'
+new_directory = '/Users/kelley/Data/Waveform/MIMICII_Sampled'
+
+record_to_info = dict()
+
+#Load sample information
+f = open(mimic_directory + '/ALARMS', 'r')
+for line in f:
+    pieces = line.strip().split(',')
+    file = pieces[0]
+    record = file.split('_')[0]
+    alarm_type = pieces[1]
+    true_alarm = pieces[2]
+
+    if record not in record_to_info:
+        record_to_info[record] = {
+                'Ventricular_Tachycardia': [],
+                'Asystole': [],
+                'Tachycardia': [],
+                'Ventricular_Flutter_Fib': [],
+                'Bradycardia': []
+            }
+    record_to_info[record][alarm_type].append((file, true_alarm))
+f.close()
+
+
+
+alarm_file = open(new_directory + '/ALARMS', 'w+')
+record_file = open(new_directory + '/RECORDS', 'w+')
+
+#Choose only three alarms of each type per record
+for record, info in record_to_info.iteritems():
+    for alarm_type, alarms in info.iteritems():
+        if len(alarms) <= 3:
+            chosen_alarms = alarms
+        else:
+            shuffle(alarms)
+            chosen_alarms = alarms[:3]
+
+        for alarm in chosen_alarms:
+            copyfile(mimic_directory + '/' + alarm[0] + '.hea', new_directory + '/' + alarm[0] + '.hea')
+            copyfile(mimic_directory + '/' + alarm[0] + '.mat', new_directory + '/' + alarm[0] + '.mat')
+            alarm_file.write(alarm[0] + ',' + alarm_type + ',' + alarm[1] + '\n')
+            record_file.write(alarm[0] + '\n')
+
+alarm_file.close()
+record_file.close()
